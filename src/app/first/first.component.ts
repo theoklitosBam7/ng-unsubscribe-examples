059fd9d8d78b1eda3d2db56c6a8300bed328cfb8
theoklitosBam7 <th.bampouris@gmail.com>
2021-03-23 18:54:14 +0200

    feat: use DestroyService

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { timer } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { MessageService } from '../message.service';
+import { DestroyService } from '../destroy.service';
 
 @Component({
   selector: 'app-first',
   template: `<p>first component works!</p>`,
+  providers: [DestroyService],
 })
 export class FirstComponent implements OnInit {
-  timer1$ = timer(0, 1000);
+  private timer1$ = timer(0, 1000);
+  private timer2$ = timer(0, 2500);
 
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private messageService: MessageService,
+    private readonly destroy$: DestroyService
+  ) {}
 
   ngOnInit(): void {
-    this.timer1$.subscribe((val) =>
-      this.messageService.add(`FirstComponent timer1$: ${val}`)
-    );
+    this.timer1$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (val) => this.messageService.add(`FirstComponent timer1$: ${val}`),
+        (err) => console.error(err),
+        () => this.messageService.add(`>>> FirstComponent timer1$ completed`)
+      );
+
+    this.timer2$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (val) => this.messageService.add(`FirstComponent timer2$: ${val}`),
+        (err) => console.error(err),
+        () => this.messageService.add(`>>> FirstComponent timer2$ completed`)
+      );
   }
 }
