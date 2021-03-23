eb9c6a7c714fd84bd5312bb976261baff6bbd2eb
theoklitosBam7 <th.bampouris@gmail.com>
2021-03-23 17:30:08 +0200

    feat: show memory leak problem

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-first',
   template: `<p>first component works!</p>`,
 })
 export class FirstComponent implements OnInit {
-  constructor() {}
+  timer1$ = timer(0, 1000);
 
-  ngOnInit(): void {}
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.timer1$.subscribe((val) =>
+      this.messageService.add(`FirstComponent timer1$: ${val}`)
+    );
+  }
 }
