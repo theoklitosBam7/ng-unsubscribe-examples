40383c504bbcee04c67deb662312ca23029396c9
theoklitosBam7 <th.bampouris@gmail.com>
2021-03-23 18:54:14 +0200

    feat: use takeUntil operator

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { timer } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, timer } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { MessageService } from '../message.service';
 
@@ -7,14 +8,34 @@ import { MessageService } from '../message.service';
   selector: 'app-first',
   template: `<p>first component works!</p>`,
 })
-export class FirstComponent implements OnInit {
-  timer1$ = timer(0, 1000);
+export class FirstComponent implements OnInit, OnDestroy {
+  private timer1$ = timer(0, 1000);
+  private timer2$ = timer(0, 2500);
+
+  private destroy$ = new Subject<void>();
 
   constructor(private messageService: MessageService) {}
 
   ngOnInit(): void {
-    this.timer1$.subscribe((val) =>
-      this.messageService.add(`FirstComponent timer1$: ${val}`)
-    );
+    this.timer1$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (val) => this.messageService.add(`FirstComponent timer1$: ${val}`),
+        (err) => console.error(err),
+        () => this.messageService.add(`>>> FirstComponent timer1$ completed`)
+      );
+
+    this.timer2$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        (val) => this.messageService.add(`FirstComponent timer2$: ${val}`),
+        (err) => console.error(err),
+        () => this.messageService.add(`>>> FirstComponent timer2$ completed`)
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
